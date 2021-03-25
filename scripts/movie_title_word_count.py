083394c5821e924ad8905c3d17b7b5c7492da0cb
Ben Platten <benplatten@Bens-MacBook-Pro.local>
2021-03-25 12:06:58 +0100

    word count fixed

--- scripts/movie_title_word_count.py
+++ scripts/movie_title_word_count.py
@@ -9,9 +9,7 @@ import time
 import numpy as np
 import nltk
 from nltk.corpus import stopwords
-nltk.download('stopwords')
-from nltk.tokenize import word_tokenize
-
+sw = stopwords.words("english")
 
 conf = SparkConf().setMaster("local").setAppName("WordCount")
 sc = SparkContext(conf = conf)
@@ -21,33 +19,29 @@ data_folder = '/Users/benplatten/workspace/UvA_BD/bigdata/data/'
 
 # helper functions
 def titleTokeniser(title):
+    title = title.split(';') 
+    title = str(title[1])
     title = ''.join([i for i in title if not i.isdigit()])
-    title = [x.strip() for x in title.split()]
-    title = [re.sub(r'\W+', '', i) for i in title]
+    title = title.lower().split()
+    title = [re.sub(r'\W+', '', word) for word in title if not word in sw] 
     title = list(filter(None, title))
-    title = [word for word in title if not word in stopwords.words()]
     return title
-   
-def parseLine(line):
-    fields = line.split(';') 
-    title = str(fields[1]) 
-    title = titleTokeniser(title.lower)
-    return (title)
-
 
 # start the clock
 start = time.time()
 
 lines = sc.textFile(data_folder + "ml-latest-small/movies.csv")
-rdd = lines.flatMap(parseLine)
+words = lines.flatMap(titleTokeniser)
 
-wordCounts = rdd.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + y)
+wordCounts = words.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + y)
 wordCountsSorted = wordCounts.map(lambda x: (x[1], x[0])).sortByKey(ascending=False)
 
 results = wordCountsSorted.collect()
 
 for result in results[0:15]:
-    print(result)
+    word = result[1].encode('ascii', 'ignore')
+    if (word):
+        print(result)
 
 # stop clock
 end = time.time()
