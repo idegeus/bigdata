05993349a5d4f80903d5204246bc729c120eecef
Shemmer93 <53403216+Shemmer93@users.noreply.github.com>
2021-04-02 20:46:14 +0200

    latest version including stage 1 partition

--- scripts/average_rating_genre.py
+++ scripts/average_rating_genre.py
@@ -1,3 +1,7 @@
+import time
+# start the clock
+start = time.time()
+
 import findspark
 findspark.init()
 findspark.find()
@@ -5,20 +9,20 @@ findspark.find()
 from pyspark import SparkConf, SparkContext
 import collections
 import re
-import time
 
 conf = SparkConf().setMaster("local").setAppName("AverageRating")
-sc = SparkContext(conf = conf)
+sc = SparkContext.getOrCreate(conf = conf.set("spark.driver.maxResultSize", "4g"))
 
-data_folder = '/Users/benplatten/workspace/UvA_BD/bigdata/data/'
+data_folder = os.getcwd() + '/Documents/UvA/Big Data/uva-bigdata-course-2021-students/src/main/data/'
 
 
 # helper functions
 def loadMovieGenres():
     movieGenres = {}
-    with open(data_folder + "ml-latest-small/movies.csv", "r", encoding='ISO-8859-1', errors='ignore') as f:
+    with open(data_folder + "ml-latest/movies.csv", "r", encoding='ISO-8859-1', errors='ignore') as f:
         for line in f:
-            fields = line.split(';')
+            fields = re.sub(r'"(\d+),(\d+)"', r'\1:\2', line)
+            fields = fields.split(',')
             genres = fields[2]
             genres = genres.strip('\n')
             genres = genres.split("|")
@@ -26,20 +30,18 @@ def loadMovieGenres():
     return movieGenres
 
 def parseRating(line):
-    fields =line.split(';')
+    fields = re.sub(r'"(\d+),(\d+)"', r'\1:\2', line)
+    fields = fields.split(',')
     movieId = int(fields[1])
-    rating= float(fields[2])
+    rating = float(fields[2])
     return (movieId, rating)
 
 def broadcastJoin(movieId,rating):
     return (movieId,broadcast_id_genre.value[movieId],rating)
 
 
-# start the clock
-start = time.time()
-
 # read movies 
-rating_lines = sc.textFile(data_folder+"/ml-latest-small/ratings.csv")
+rating_lines = sc.textFile(data_folder + "/ml-latest/ratings.csv")
 # broadcast ID + genre
 broadcast_id_genre = sc.broadcast(loadMovieGenres())
 # parse reviews into RDD
@@ -49,7 +51,7 @@ id_genres_rating = ratings_rdd.map(lambda x: broadcastJoin(x[0],x[1]))
 # flatten genre lists into seperate rows
 genre_ratings = id_genres_rating.flatMap(lambda x: [(w, x[2]) for w in x[1]]).collect()
 # convert back to RDD
-g_rdd = sc.parallelize(genre_ratings)
+g_rdd = sc.parallelize(genre_ratings, numSlices=1000)
 # reduce
 ratingsByGenre = g_rdd.mapValues(lambda x: (x,1)).reduceByKey(lambda x, y: (x[0]+y[0],x[1]+y[1]))
 # calculate average
